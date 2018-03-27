cfa40e3f986c697de637770dfa783705a0093e77
Christophe <christophe@colorfield.eu>
2018-03-27 18:29:30 +0200

    Fix unavailable route in hook_checklistapi_checklist_info(), set all sections enabled by default

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -122,13 +122,6 @@ class SettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
